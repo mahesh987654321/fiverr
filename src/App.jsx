c7cac8fc76ad62fa1936c150045ff29e8149fc5b
Mahesh Biswas <maheshbiswas26@gmail.com>
2023-02-21 15:09:11 +0600

    navbaar_design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
 import "./add.scss";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
 const App = () => {
-  return <div className="add">App</div>;
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
+import { Link } from "react-router-dom";
+import "./navbar.scss";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <Link className="link" to="/">
+            <span className="text">fiverr</span>
+          </Link>
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <span>fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>sign in</span>
+          <span>Become a Seller</span>
+          <button>Join</button>
+        </div>
+      </div>
+      <hr />
+      <div className="menu">
+        <span>Test1</span>
+        <span>Test2</span>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
